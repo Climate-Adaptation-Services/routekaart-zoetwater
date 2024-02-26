68bbb5792251c06fa0602ba3c10f25e943ffdd72
stichtingcas <daniel@climateadaptationservices.com>
2024-02-26 09:38:19 +0100

    Update TooltipSpoor.svelte

--- src/lib/components/TooltipSpoor.svelte
+++ src/lib/components/TooltipSpoor.svelte
@@ -8,12 +8,17 @@
   
   $: bbox = document.getElementsByClassName(`circle-${$spoorSelection['Korte titel'].replaceAll(' ','')}`)[0].getBoundingClientRect()
 
+  const maand = {
+    '01':'Januari', '02':'Februari', '03':'Maart', '04':'April', '05':'Mei', '06':'Juni', '07':'Juli', '08':'Augustus', '09':'September', '10':'Oktober', '11':'November', '12':'December'
+  }
+
 </script>   
 
 <div class='tooltip-spoor' style='left:{(bbox.left > w*0.6) ? bbox.left - 500 : bbox.left}px; top:{bbox.top-250}px; border:5px solid {(spoor === 'bpz') ? '#EA7722' : '#6FAD33'}'>
   <h4>{$spoorSelection['Volledige titel']}</h4>
   <p style='font-size:14px'>{$spoorSelection['Korte beschrijving']}</p>
-  <p style='font-size:12px'><em>{$spoorSelection['Type']}</em></p>
+  <p style='font-size:12px; width:50%; float:left'><em>{$spoorSelection['Type']}</em></p>
+  <p style='font-size:12px; display:inline-block; float:left; text-align:right; width:50%'><em>{maand[$spoorSelection['Datum'].split('-')[1]] + ' ' + $spoorSelection['Datum'].split('-')[0]}</em></p>
 </div>
 
 <style>
